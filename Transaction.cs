11336b8dd9883d7fabca66c5b3392ec72cc899eb
Shan Konduru <ShanKonduru@users.noreply.github.com>
2022-04-29 08:49:05 +0530

    Blockchain with C#

---
+++ Transaction.cs
@@ -0,0 +1,13 @@
+namespace NewBlockchainInCSharp {
+
+    public class Transaction {
+        public string From { get; }
+        public string To { get; }
+        public double Amount { get; }
+        public Transaction (string from, string to, double amount) {
+            From = from;
+            To = to;
+            Amount = amount;
+        }
+    }
+}
